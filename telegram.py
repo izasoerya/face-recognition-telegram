3f9ce12308486324770cd873a212f6d7bddb9cf3
izasoerya <ihza.surya@gmail.com>
2024-02-14 23:30:22 +0700

    feat: add telegram bot send message

---
+++ telegram.py
@@ -0,0 +1,8 @@
+import requests
+
+def send_telegram_message (message_content):
+    TOKEN = "6504288990:AAHfOe84wICn4zbwWcX0iNrRbBK4wSoFHvk"
+    chat_id = "6720768632"
+    message = message_content
+    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={message}"
+    print(requests.get(url).json())
